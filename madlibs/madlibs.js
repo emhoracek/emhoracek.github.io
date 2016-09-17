63ddb0c4e298fadca4c703c4e05ee14e3c353dc3
Libby Horacek <libby@daydrea.me>
2016-09-17 13:14:34 -0400

    update madlibs

--- madlibs/madlibs.js
+++ madlibs/madlibs.js
@@ -1,23 +1,23 @@
 /*
  * MADLIBS!! by Libby
- * for Flocabulary 
+ * for Flocabulary
  *
  */
 
 // Make a story!
 // It's easier if you don't have to use lots of ugly HTML tags.
-// So here's a very pretty syntax for making madlibs! Just wrap every 
-// "mad" word in braces. The first word is the original word, and the 
+// So here's a very pretty syntax for making madlibs! Just wrap every
+// "mad" word in braces. The first word is the original word, and the
 // second is your description.
-var sample_story = 
-"JavaScript is built on some very { good, adjective } ideas and a few very " + 
-"{ bad, adjective } ones. The very { good, adjective } ideas include " + 
+var sample_story =
+"JavaScript is built on some very { good, adjective } ideas and a few very " +
+"{ bad, adjective } ones. The very { good, adjective } ideas include " +
 "{ functions, plural noun }, { loose, adjective } typing, " +
-"{ dynamic, adjective }  objects, and an expressive object literal " + 
+"{ dynamic, adjective }  objects, and an expressive object literal " +
 "{ notation, noun }. The { bad, adjective } ideas include a programming " +
 "model based on global { variables, plural noun }. JavaScript’s functions " +
 "are first class  { objects, plural noun } with (mostly) " +
-"{ lexical, adjective }  scoping. ";
+    "{ lexical, adjective }  scoping. ";
 
 // Next some resources for parsing the story into things we can use with React.
 
@@ -34,30 +34,80 @@ var parse_story = function(story) {
     var match_slow = /([^\{]+)\{\s?([^,]+),\s?([^\}]+)\s?\}(.*$)/
     var match = match_slow.exec(story);
     for (var i = 0; match != null; i++) {
-      var plain = match[1];
-      var madword = { num: i.toString(), original: match[2], current: "", type: match[3] };
+      var plain = { type: "text",
+                    text: match[1] };
+      var madword = { type: "mad",
+                      original: match[2],
+                      current: "",
+                      part_of_speech: match[3] };
       text.push( plain );
       text.push( madword );
       rest_of_story = match[4];
       match = match_slow.exec(rest_of_story);
     }
-    text.push(rest_of_story);
+    text.push({ type: "text",
+                text: rest_of_story });
     return text;
 }
 
 // Now for the React!!
 
+
+var MadPreview = React.createClass({
+  textToStory: function(text) {
+    console.log(text);
+    story = text.split(' ').map(function (word) {
+      return { type: "text",
+               text: word }
+    });
+    return story;
+  },
+  render: function() {
+    return (
+        <div className="preview">
+          <MadStory story={this.textToStory(this.props.text)} visible="true"  />
+        </div>
+    )
+  }
+});
+
+var MadEditor = React.createClass({
+  getInitialState: function(){
+    return {
+      text: "",
+      story: []
+    }
+  },
+  handleChange: function (e) {
+    console.log(this.props);
+    console.log(e.target.value);
+    this.setState ({
+      text: e.target.value
+    });
+  },
+  render: function(){
+    return (
+      <div className="editor">
+        <textarea onChange={ this.handleChange }/>
+        <MadPreview text={ this.state.text } />
+      </div>
+    )
+  }
+});
+
 var Prompt = React.createClass({
   handleChange: function(e) {
-    this.props.onChange({ word_id: this.props.word.num, 
+    console.log(this.props);
+    console.log(e.target.value);
+    this.props.onChange({ word_id: this.props.word_id,
                           new_word: e.target.value});
   },
   render: function(){
     return (
       <li>
-      <label className="prompt-label">{this.props.word.type}</label>
+      <label className="prompt-label">{this.props.word.part_of_speech}</label>
       <input type="text"
-             onChange={this.handleChange} 
+             onChange={this.handleChange}
              value={this.props.word.current}
              className="prompt-input"/>
       </li>
@@ -66,14 +116,18 @@ var Prompt = React.createClass({
 });
 
 var SetOfPrompts = React.createClass({
-  handleChange: function(word){
-    this.props.onChange(word);
-  },
   render: function () {
-    var prompts = this.props.children;
+    var that = this;
+    var prompts = this.props.words.map(function(word, i) {
+      if (word.type === "mad") {
+        return (
+            <Prompt key={i} word={word} word_id={i} onChange={that.props.onChange} onClear={that.props.onClear} />
+        )
+      }
+    });
     return (
       <ul className="list-prompts">
-      { prompts }
+        { prompts }
       </ul>
       )
   }
@@ -81,20 +135,21 @@ var SetOfPrompts = React.createClass({
 
 var MadWord = React.createClass({
   render: function() {
-    return <span className="madword">{this.props.word.current}</span>
+    var type = this.props.word.type
+    var word = this.props.word;
+    if (type == "text") {
+      return <span>{ word.text } </span>
+    } else {
+      return <span className="madword">{ word.current }</span>
+    }
   }
 });
 
 var MadStory = React.createClass({
   render: function(){
     var story = this.props.story;
-    story = story.map(function(element) {
-      if ((typeof element) == "string") {
-        return <span>{ element }</span>
-      }
-      else {
-        return <MadWord key={element.num} word={element} />
-      }
+    story = story.map(function(word, i) {
+      return <MadWord key={i} type={word.type} word={word} />
     });
     if (this.props.visible) {
       var visibleClass = "madstory";
@@ -104,7 +159,7 @@ var MadStory = React.createClass({
     }
     return (
       <p className={ visibleClass }>
-      { story }
+        { story }
       </p>
     )
   }
@@ -119,25 +174,26 @@ var MadLibs = React.createClass({
   },
   clearPrompts: function() {
     this.setState ({
-      story: this.state.story.map( function (x) {
-        if ((typeof x) != "string") {
-          x.current = "";
+      story: this.state.story.map( function (word) {
+        if (word.type === "mad") {
+          word.current = "";
         }
         return x;
       })
     })
   },
   changeStory: function(story_text, word_id, new_word) {
-    var story = story_text.map ( function (x) {
-      if (x.num == word_id) {
-        x.current = new_word;
+    var story = story_text.map ( function (word, i) {
+      if (i == word_id) {
+        word.current = new_word;
       }
-      return x;
+      return word;
     });
     return story;
   },
-  handleChange: function(word) {
-    var newStory = this.changeStory(this.state.story, word.word_id, word.new_word);
+  handleChange: function(word_info) {
+    console.log(word_info);
+    var newStory = this.changeStory(this.state.story, word_info.word_id, word_info.new_word);
     this.setState ({
       story: newStory
     });
@@ -155,50 +211,21 @@ var MadLibs = React.createClass({
     }
   },
   render: function() {
-    var words = this.state.story.filter(function(x) { return ((typeof x) != "string") });
-    if (this.state.visible) {
-      var submit = "hide";
-    }
-    else {
-      var submit = "show";
-    }
-    var that = this;
-    var prompts = words.map(function(word) {
-      return (
-        <Prompt key={word.num} word={word} onChange={that.handleChange} onClear={that.clearPrompts} />
-      )});
+    var words = this.state.story.filter(function(x) { return (x.type === "mad") });
     return (
       <div className="madlibs">
-        <SetOfPrompts words={words} onChange={this.handleChange}> { prompts } </SetOfPrompts>
-        <button onClick={this.toggleVisibility}> {submit} </button>
+        <SetOfPrompts words={this.state.story} onChange={this.handleChange} onClear={this.clearPrompts} />
+        <button onClick={this.toggleVisibility}> { this.state.visible ? "hide" : "show" } </button>
         <button onClick={this.clearPrompts}> clear </button>
         <MadStory story={this.state.story} visible={this.state.visible}  />
+        <MadEditor />
       </div>
-      )
+    )
   }
 });
 
-var onChange = function(self){
-    var words = self.state.story.filter(function(x) { return ((typeof x) != "string") });
-    if (self.state.visible) {
-      var submit = "hide";
-    }
-    else {
-      var submit = "show";
-    }
-    var that = self;
-    var prompts = words.map(function(word) {
-      return (
-        <Prompt key={word.num} word={word} onChange={that.handleChange} onClear={that.clearPrompts} />
-      )});
-    React.render (
-      <div className="madlibs">
-        <SetOfPrompts words={words} onChange={self.handleChange}> { prompts } </SetOfPrompts>
-        <button onClick={self.toggleVisibility}> {submit} </button>
-        <button onClick={self.clearPrompts}> clear </button>
-        <MadStory story={self.state.story} visible={self.state.visible}  />
-      </div>
-      )
-};
-
 React.render(<MadLibs />, document.getElementById('madlibs'));
+
+if (typeof window !== 'undefined') {
+    window.React = React;
+}
